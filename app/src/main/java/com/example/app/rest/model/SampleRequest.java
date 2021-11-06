46fd6ec4309a5c5e1d1c501abb6c0160186d8d63
akito <akito@USERnoMacBook-ea.local>
2021-11-06 21:41:12 +0900

    implement correlation check

--- app/src/main/java/com/example/app/rest/model/SampleRequest.java
+++ app/src/main/java/com/example/app/rest/model/SampleRequest.java
@@ -1,7 +1,10 @@
 package com.example.app.rest.model;
 
+import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class SampleRequest {
 
@@ -9,13 +12,29 @@ public class SampleRequest {
 	@Size(min = 6, max = 6, message = "半角で6文字の入力が必要")
 	private String businessData;
 
+	
+	@NotNull(message = "値の入力が必要")
+	private String correlationData;
+	
 	public String getBusinessData() {
 		return businessData;
 	}
 
 	public void setBusinessData(String businessData) {
 		this.businessData = businessData;
-	}
+	}	
 	
+	public String getCorrelationData() {
+		return correlationData;
+	}
+
+	public void setCorrelationData(String correlationData) {
+		this.correlationData = correlationData;
+	}
+
+	@AssertTrue(message = "値の入力が両方とも必要")
+	public boolean isCorrelationCheck() {
+		return StringUtils.isNoneEmpty(businessData, correlationData);
+	}
 	
 }
