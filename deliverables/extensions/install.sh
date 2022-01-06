5ae3accaabcdfacda0d428eec7f73fd3c7f51428
kumaki <8kpffr@gmail.com>
2022-01-07 01:41:24 +0900

    test sript

--- deliverables/extensions/install.sh
+++ deliverables/extensions/install.sh
@@ -2,6 +2,7 @@
 
 injected_dir=$1
 echo ${injected_dir}
+sleep 300s
 source /usr/local/s2i/install-common.sh
 install_deployments ${injected_dir}/injected-deployments.war
 install_modules ${injected_dir}/modules
